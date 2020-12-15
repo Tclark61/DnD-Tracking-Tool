2c69df9f5fbf5ba3538187f5efa3b1055db99bf9
gargar77 <gbautista2@knights.ucf.edu>
2020-12-15 15:37:55 -0500

    added db seed and connection test

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SQLite from 'expo-sqlite';
+import { addTables, seedMonstersTable} from './db/seed';
 
+const db = SQLite.openDatabase("dnd_tracker.db");
 
+const showTestResult = () => {
+  db.transaction( tx => {
+    tx.executeSql(
+      "SELECT * FROM monsters LIMIT 1",
+      [],
+      (tx,resultSet) => console.log(resultSet))
+  })
+};
+
+if(db) {
+  // addTables(db);
+  // seedMonstersTable(db);
+  showTestResult();
+} else {
+  console.log('db not found!')
+}
 export default function App() {
   return (
     <View style={styles.container}>
