89e088da32d3a859011539c64f7b2b024781ddf2
gargar77 <gbautista2@knights.ucf.edu>
2020-12-21 16:52:56 -0500

    converted functional component into clas component && disabled DB testing

--- App.js
+++ App.js
@@ -1,36 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as SQLite from 'expo-sqlite';
 import { addTables, seedMonstersTable} from './db/seed';
 
 const db = SQLite.openDatabase("dnd_tracker.db");
 // DB test [START]
-const showTestResult = () => {
-  db.transaction( tx => {
-    tx.executeSql(
-      "SELECT * FROM monsters LIMIT 1",
-      [],
-      (tx,resultSet) => console.log(resultSet))
-  })
-};
+// const showTestResult = () => {
+//   db.transaction( tx => {
+//     tx.executeSql(
+//       "SELECT * FROM monsters LIMIT 1",
+//       [],
+//       (tx,resultSet) => console.log(resultSet))
+//   })
+// };
 
-if(db) {
-  // addTables(db);
-  // seedMonstersTable(db);
-  showTestResult();
-} else {
-  console.log('db not found!')
-}
+// if(db) {
+//   // addTables(db);
+//   // seedMonstersTable(db);
+//   showTestResult();
+// } else {
+//   console.log('db not found!')
+// }
 // DB test [END]
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
@@ -41,3 +44,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
